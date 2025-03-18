701db0682977384f842fa829e7d081f10297feb7
danez13 <hernandezdaniel1702@gmail.com>
2025-03-18 10:17:52 -0400

    Improve file upload handling and enhance user feedback in app.py

--- app.py
+++ app.py
@@ -8,23 +8,27 @@ if __name__ == "__main__":
     st.title("ML HUB")
 
     uploaded_file = st.file_uploader("Upload a file", type=["csv"])
-
-    if uploaded_file is None:
-        pass
+    if not uploaded_file:
+        st.warning("please upload a dataset file to access more features")
     elif not backend.load_data(uploaded_file):
         if backend.data_isempty():
             st.error("you have uploaded an empty file, please upload again")
         st.stop()
     else:
-        st.success("file upload was successful")
+        file_upload_success = st.success("file upload was successful")
     
     dataset_tab, model_tab, train_tab = st.tabs(["Dataset", "Model", "Train"])
     with dataset_tab:
         st.subheader("Dataset processing")
+
+        if uploaded_file is None:
+            st.stop()
+
         with st.expander("dataset configuration"):
             pass
-        dataset = backend.get_data()
-        st.write(dataset if dataset else '')
+        
+        st.write(backend.get_data())
+
 
     with model_tab:
         st.subheader("Model configuration")
@@ -38,17 +42,20 @@ if __name__ == "__main__":
 
     with train_tab:
         st.subheader("Model training")
+        
         config = backend.get_user_config()
         st.write("Task:",config["task"]if config["task"] else '')
         st.write("Features:", ",".join(config["features"]))
         st.write("Target:", config["target"] if config["target"] else '')
         st.write("Model:", backend.get_model() if backend.get_model() else '')
+        
         if not config["task"] and not config["target"] and len(config["features"])==0:
             st.warning("please set model configuration")
-        else:
-            if st.button("Train model"):
-                st.divider()
-                if backend.init_model_training():
-                    st.success("model training was a success")
-                    st.write("Accuracy score:",backend.get_model_score())
+            st.stop()
+        
+        if st.button("Train model"):
+            st.divider()
+            if backend.init_model_training():
+                st.success("model training was a success")
+                st.write("Accuracy score:",backend.get_model_score())
 
