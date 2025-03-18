980aac0538f16ba3d6123313092c852261766cdf
Daniel Hernandez <47952730+danez13@users.noreply.github.com>
2025-03-17 23:29:22 -0400

    Separate backend frontend and runtime functionality (#23)
    * Refactor ML HUB application structure by removing main.py and introducing app.py, data.py, and server.py for improved organization and functionality
    
    * Update README.md to include core features and functionalities of ML Hub

--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 * __Objective:__ The platform will enable data scientists and machine learning practitioners to store and process datasets. as well as train, and evaluate machine learning models all in one place.
 
 * __Target Audience:__ Data scientists, machine learning engineers, researchers, and developers.
+
+### Core Features and Functionalities
+1. Dataset Management
+    * __upload datasets:__ Users can upload various types of dataset
+2. Model Management
+   * __Create Models:__ Users can define, and create models
+   *  __Training:__ Train models on the datasets uploaded by users.
+   * __Model Evaluation:__ Evaluate the models using standard metrics.
---
+++ server.py
@@ -0,0 +1,44 @@
+import app
+import data
+import streamlit as st
+
+class connection_handler():
+    def __init__(self):
+        self._data_connection = data.data_handler()
+        self._model_connection = data.model_handler()
+
+    def isdata(self):
+        return self._data_connection.isempty()
+        
+    def load_data(self,file):
+        if self._data_connection.load(file):
+            return True
+        else:
+            return self.isdata() and file
+    
+    def load_model(self):
+        return self._model_connection.load()
+    
+    def init_model_training(self):
+        return self._model_connection.train_model(self._data_connection.get_data())
+
+    def get_model_score(self):
+        return self._model_connection.eval_model(self._data_connection.get_data())
+
+    def data_isempty(self):
+        return self._data_connection.isempty()
+
+    def get_data(self):
+        return self._data_connection.get_data()
+    
+    def get_columns(self):
+        return self._data_connection.get_columns()
+
+    def get_model(self):
+        return self._model_connection.get_model()
+
+    def get_user_config(self):
+        return self._model_connection.get_config()
+    
+    def set_user_config(self,task,features,target):
+        return self._model_connection.set_config(task,features,target)
